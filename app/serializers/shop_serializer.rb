055fd2e8304864e70580b25411ec2c1d1c88c745
Olivia Pomeroy <opomeroy26@gmail.com>
2022-05-11 12:29:00 -0700

    Working model

--- app/serializers/shop_serializer.rb
+++ app/serializers/shop_serializer.rb
@@ -1,7 +1,7 @@
 class ShopSerializer < ActiveModel::Serializer
   attributes :id, :name, :pricing, :wifi, :rating, :likes, :longitude, :latitude
   # has_one :user
-  has_many :comments 
+  has_many :comments
   has_many :bookmarks
 
 #   def comments
--- client/src/components/AddShop.js
+++ client/src/components/AddShop.js
@@ -95,8 +95,8 @@ function handleShopFormSubmit(e) {
                                             name='wifi'
                                             value={shopForm.wifi}
                                             onChange={handleChange}>
-                                                <option value="true">Yes</option>
-                                                <option value="false">No</option>
+                                                <option value={true}>Yes</option>
+                                                <option value={false}>No</option>
                                         </select>
                                     </div>
                                 </div>
